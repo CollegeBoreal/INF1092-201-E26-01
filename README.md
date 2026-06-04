50a32ad549d16b8d438782e37958906e171b8bb1
Pierre Bob Adly <Bobadly.pierre@monboreal.ca>
2026-06-03 22:05:23 -0400

    TP Terraform

---
+++ README.md
@@ -0,0 +1,16 @@
+# Projet IaC - OpenTofu
+
+## Objectif
+Créer une machine virtuelle automatiquement avec OpenTofu sur Proxmox.
+
+## Technologies
+- OpenTofu
+- Proxmox
+- Cloud-init
+
+## Description
+Ce projet permet de déployer une VM Ubuntu avec configuration réseau et accès SSH automatisé.
+
+## Auteur
+Bob Adly Pierre
+
---
+++ main.tf
@@ -0,0 +1,26 @@
+resource "proxmox_vm_qemu" "vm1" {
+  name        = var.pm_vm_name
+  target_node = "labinfo"
+  clone       = "ubuntu-jammy-template"
+
+  cores   = 2
+  memory  = 2048
+
+  disk {
+    size    = "10G"
+    type    = "scsi"
+    storage = "local-lvm"
+  }
+
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+
+  os_type = "cloud-init"
+
+  ipconfig0 = var.pm_ipconfig0
+  nameserver = var.pm_nameserver
+
+  ciuser  = "ubuntu"
+}
